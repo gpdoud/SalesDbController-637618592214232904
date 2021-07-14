48c07235bec48b4ede33ee8de2f58323a007ccb0
Greg Doud <gpdoud@gmail.com>
2021-07-14 11:33:44 -0400

    Committed by GitUtil

---
+++ SalesDbController/Migrations/20210624193135_init.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesDbController.Migrations {
+    public partial class init : Migration {
+        protected override void Up(MigrationBuilder migrationBuilder) {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder) {
+        }
+    }
+}
---
+++ SalesDbController/Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesDbController.Models {
+    
+    public class AppDbContext : DbContext {
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> Orderlines { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+
+        }
+    }
+}
